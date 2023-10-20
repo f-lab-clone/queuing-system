6da1a8687577d5479c7206e6dd9e480de205117b
junha-ahn <junha.ahn.dev@gmail.com>
2023-10-20 13:38:22 +0900

    test: fix test for look easy

--- test/integrationTest/api/ticket.spec.js
+++ test/integrationTest/api/ticket.spec.js
@@ -9,6 +9,16 @@ describe('Ticket', () => {
   let redis = null
   let ticketStoreService = null
 
+  const testEventId = 1
+  const testUserId = 1
+
+  async function queueMovingJob(eventId, count) {
+    const tickets = await ticketStoreService.shiftFromWaiting(eventId, count)
+    for (const { value, score } of tickets) {
+      await ticketStoreService.pushIntoRunning(eventId, value, score)
+    }
+  }
+
   beforeAll(async () => {
     container = await new GenericContainer('redis')
       .withExposedPorts(6379)
@@ -28,42 +38,34 @@ describe('Ticket', () => {
   beforeEach(async () => {
     await redis.flushAll()
   })
-
-  async function queueMovingJob(eventId, count) {
-    const tickets = await ticketStoreService.shiftFromWaiting(eventId, count)
-    for (const { value, score } of tickets) {
-      await ticketStoreService.pushIntoRunning(eventId, value, score)
-    }
-  }
-
   describe('POST /ticket 은', () => {
     describe('성공시', () => {
       it('전체 Queue를 관리하는 Sorted Set에 EventId를 추가한다', async () => {
-        for (let i = 1; i < 10; i++) {
+        for (let testEventId = 1; testEventId < 10; testEventId++) {
           await chai.request(server).post('/ticket').send({
-            eventId: i,
-            userId: 1,
+            eventId: testEventId,
+            userId: testUserId,
           })
           const res = await redis.zRank(
             ticketStoreService.getQueueKey(),
-            i.toString(),
+            testEventId.toString(),
           )
-          expect(res).to.deep.equal(i - 1)
+          expect(res).to.deep.equal(testEventId - 1)
         }
       })
 
       it('201을 리턴한다', async () => {
         const res = await chai.request(server).post('/ticket').send({
-          eventId: 1,
-          userId: 100,
+          eventId: testEventId,
+          userId: testUserId,
         })
         expect(res).to.have.status(201)
         expect(res.body.status).to.deep.equal(true)
       })
       it('{eventId, userId, isWaiting, timestamp, offset}을 반환한다', async () => {
         const res = await chai.request(server).post('/ticket').send({
-          eventId: 1,
-          userId: 100,
+          eventId: testEventId,
+          userId: testUserId,
         })
         expect(res.body.data).to.be.an('object')
         expect(res.body.data).to.have.property('eventId')
@@ -72,12 +74,12 @@ describe('Ticket', () => {
         expect(res.body.data).to.have.property('offset')
       })
       it('대기열은 정확한 Offset을 반환한다', async () => {
-        for (let i = 1; i < 10; i++) {
+        for (let testUserId = 1; testUserId < 10; testUserId++) {
           const res = await chai.request(server).post('/ticket').send({
-            eventId: 1,
-            userId: i,
+            eventId: testEventId,
+            userId: testUserId,
           })
-          expect(res.body.data.offset).to.deep.equal(i - 1)
+          expect(res.body.data.offset).to.deep.equal(testUserId - 1)
         }
       })
     })
@@ -87,22 +89,26 @@ describe('Ticket', () => {
     describe('성공시', () => {
       it('200을 리턴한다', async () => {
         await chai.request(server).post('/ticket').send({
-          eventId: 1,
-          userId: 100,
+          eventId: testEventId,
+          userId: testUserId,
         })
 
-        const res = await chai.request(server).get(`/ticket/1/100`)
+        const res = await chai
+          .request(server)
+          .get(`/ticket/${testEventId}/${testUserId}`)
         expect(res).to.have.status(200)
         expect(res.body.status).to.deep.equal(true)
       })
 
       it('{eventId, userId, isWaiting, timestamp, offset}을 반환한다', async () => {
         await chai.request(server).post('/ticket').send({
-          eventId: 1,
-          userId: 100,
+          eventId: testEventId,
+          userId: testUserId,
         })
 
-        const res = await chai.request(server).get(`/ticket/1/100`)
+        const res = await chai
+          .request(server)
+          .get(`/ticket/${testEventId}/${testUserId}`)
         expect(res.body.data).to.be.an('object')
         expect(res.body.data).to.have.property('eventId')
         expect(res.body.data).to.have.property('userId')
@@ -111,27 +117,30 @@ describe('Ticket', () => {
       })
 
       it('Running Queue Ticket은 IsWaiting = True를 반환한다.', async () => {
-        for (let i = 1; i < 10; i++) {
+        for (let testUserId = 1; testUserId < 10; testUserId++) {
           await chai.request(server).post('/ticket').send({
-            eventId: 1,
-            userId: i,
+            eventId: testEventId,
+            userId: testUserId,
           })
         }
 
-        await queueMovingJob(1, 5)
+        const FIVE_PEOPLE = 5
+        await queueMovingJob(testEventId, FIVE_PEOPLE)
 
-        const res1 = await chai.request(server).get(`/ticket/1/1`)
+        const res1 = await chai.request(server).get(`/ticket/${testEventId}/1`)
         expect(res1.body.data.isWaiting).to.deep.equal(false)
         expect(res1.body.data.offset).to.deep.equal(0)
 
-        const res2 = await chai.request(server).get(`/ticket/1/6`)
+        const res2 = await chai.request(server).get(`/ticket/${testEventId}/6`)
         expect(res2.body.data.isWaiting).to.deep.equal(true)
         expect(res2.body.data.offset).to.deep.equal(0)
       })
     })
     describe('실패시', () => {
       it('대기열 티켓이 없을 경우 404 Not Found를 리턴한다', async () => {
-        const res = await chai.request(server).get(`/ticket/1/1`)
+        const res = await chai
+          .request(server)
+          .get(`/ticket/${testEventId}/${testUserId}`)
         expect(res).to.have.status(404)
         expect(res.body.status).to.deep.equal(false)
       })
